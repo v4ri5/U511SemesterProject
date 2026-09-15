305469813d1fe8c7ef41309f44e968532441b23c
v4ri5 <tavarisct123@gmail.com>
2026-09-15 11:16:01 -0400

    Remove commented-out process examples from TestPriority.py

--- TestPriority.py
+++ TestPriority.py
@@ -2,15 +2,6 @@ from Process import Process
 from harvest import harvest_processes
 from PrioritySchedule import priority_scheduling
 processes, dropped = harvest_processes()
-
-"""
-processes = [
-    Process(0, 4, 5, 1, 3),
-    Process(1, 4, 5, 2, 2), 
-    Process(2, 4, 5, 3, 1),
-    Process(3, 100, 5, 4, 0)
-]
-"""
     
     
 print("Priority Scheduled Processes:")
